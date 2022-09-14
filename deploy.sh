8a62124d134a1246486a64824e35addfde9e0e1b
= <=>
2022-09-15 01:17:47 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-vite build && rm -rf /home/nginx/idiom2 && cp -r dist /home/nginx/idiom2 && sudo chown -R nginx:nginx /home/nginx/idiom2
+export NODE_OPTIONS=--max-old-space-size=819 && vite build && rm -rf /home/nginx/idiom2 && cp -r dist /home/nginx/idiom2 && sudo chown -R nginx:nginx /home/nginx/idiom2
